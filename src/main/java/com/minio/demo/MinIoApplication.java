3f11270d7d9909aabd97805dcc93e9e40e63ae90
Gatsby <caijinyang98@gmail.com>
2022-02-21 20:12:31 +0800

    style: added author

--- src/main/java/com/minio/demo/MinIoApplication.java
+++ src/main/java/com/minio/demo/MinIoApplication.java
@@ -3,9 +3,11 @@ package com.minio.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Caijinyang
+ */
 @SpringBootApplication
 public class MinIoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MinIoApplication.class, args);
     }
